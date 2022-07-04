f461780e94beb6b39bcee3f202c1bf526fc099a2
soobing <qls0147@naver.com>
2022-07-04 23:30:50 +0900

    feat: DateInNumber

---
+++ src/DateInNumber.js
@@ -0,0 +1,30 @@
+
+class DateInNumber {
+  constructor(date) {
+    this._date = isNaN(date.valueOf()) ? new Date() : date;
+  }
+
+  get year() {
+    return this._date.getFullYear();
+  }
+
+  set year(value) {
+    return this._date.setFullYear(value);
+  }
+
+  get month() {
+    return this._date.getMonth() + 1;
+  }
+
+  set month(value) {
+    this._date.setMonth(value - 1);
+  }
+
+  get day() {
+    return this._date.getDate();
+  }
+
+  set day(value) {
+    return this._date.setDate(value);
+  }
+}
