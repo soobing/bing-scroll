03920c4d273b311d22860c71fca921837783341e
soobing <qls0147@naver.com>
2022-07-18 23:45:11 +0900

    fix: dist, buil 파일 구분

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,7 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 
 export default {
-  input: "./src/index.js",
+  input: "./build/src/index.js",
   output: {
     file: "./dist/bundle.js",
     format: "es",
